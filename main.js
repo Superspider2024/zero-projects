fbd8723614a75c80e93ac770f1b7ea8de42dd9ba
superspider <diginalunlimited@gmail.com>
2024-10-13 10:25:24 +0300

    got the data

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const apiKey='39878118c3bd5269e808d6ffd456a339' ;
 const city = 'Nairobi';
-fetch(`api.openweathermap.org/data/2.5/weather?q=${city}y&APPID=${apiKey}`)
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}`)
     .then(response=>{
         if(!response.ok){
-            throw new Error("Network gave up on you LOL")
+            throw new Error(`Network gave up on you LOL:${response.status}`)
         }
         return response.json()
 
@@ -12,5 +12,12 @@ fetch(`api.openweathermap.org/data/2.5/weather?q=${city}y&APPID=${apiKey}`)
         console.log(`Everything you need heheheh:  ${data}`)
     })
     .catch(error=>{
-        console.error(`to the funny error: ${error.message} lol the status ${error.status}`)
+        console.error(`to the funny error: ${error.message}`)
     })
+
+const temperatureKelvin = data.main.temp;
+const temperatureCelsius = temperatureKelvin - 273.15; 
+const description = data.weather[0].description;
+const rain = data.rain ? data.rain['1h'] || 0 : 0; 
+const humidity = data.main.humidity; 
+const cityName = data.name; 
